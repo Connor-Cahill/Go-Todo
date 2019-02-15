fd256bf8953a7c50467b0181d71adb5890ddc517
Connor Cahill <connorcahill913@gmail.com>
2019-02-14 17:35:22 -0800

    [Cleaned]: modularized server.go and setup mongodb I think

--- server.go
+++ server.go
@@ -1,15 +1,53 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/labstack/echo"
+
+	"github.com/mongodb/mongo-go-driver/mongo"
 )
 
-func main() {
+//sets up echo server
+func setupServer() {
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
+		return c.String(http.StatusOK, "Yo Yo Yo")
 	})
 	e.Logger.Fatal(e.Start(":3000"))
+}
+
+//TodoItem is the object for Todo List Items
+type TodoItem struct {
+	name        string
+	description string
+	importance  int
+}
+
+//Sets up mongodb instance
+func setupDB() {
+	client, err := mongo.Connect(context.TODO(), "mongodb://localhost:27017")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to Mongo!")
+
+	// collection := client.Database("go-todo").Collection("TodoItems")
+}
+
+//SetupServer starts the server on localhost:3000
+func main() {
+	setupServer()
+	setupDB()
+
 }
